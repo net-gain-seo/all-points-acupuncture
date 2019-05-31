dc2047d6956e6637f7615ace0e18542ba36b863e
Corbin Maynard <corbinm@netgainseo.com>
2019-05-31 17:36:07 -0400

    added google analytics and event tracking. also fixed an issue with the z-index of the banner on sub pages

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
     $(document).ready(function(){
 
         $(window).scroll(function(){
-            console.log('scroll');
             var aTop = 100;
 
             if($(this).scrollTop() >= 275){
@@ -16,23 +15,46 @@
             }else if($(this).scrollTop() <= 150){
                 $('body .site-header').removeClass('sticky');
             }
-            
         });
 
+        $(document).on('mailsent.wpcf7', function (e) {
+            $form=$(e.target);
+            
+            if($form.attr('id').search('f150')!=-1){               
+                gtag('event', 'Submit', {
+                  'event_category': 'Form',
+                  'event_label': 'Contact'
+                });
+            }
+            else if($form.attr('id').search('f191')!=-1){               
+                gtag('event', 'Submit', {
+                  'event_category': 'Form',
+                  'event_label': 'Popup'
+                });
+            }
+        });
+
+        jQuery("a[href^='tel:']").click(function(){
+            if (jQuery(window).width() <= 500){
+                var href = jQuery(this).attr("href");
+                ga('send','event','Phone','Click',href);
+                
+                gtag('event', 'Click', {
+                  'event_category': 'Phone',
+                  'event_label': href
+                });
+            }
+        });
 
         $('.quotePop').click(function(e){
             e.preventDefault();
-            console.log('click');
             $('#quotePop').modal();
         });
 
         $('.mobileMenuToggle').click(function(e){
-            console.log('click');
             e.preventDefault();
             $('#mobileNav').toggleClass('open');
         })
     
     });
-
-
 })(jQuery);
